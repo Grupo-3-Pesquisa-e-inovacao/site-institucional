3d603423b488f1eac41b4cf3ed5025f039f8e2ae
Ciliberti <arthur.coliveira@sptech.school>
2023-10-13 18:12:34 -0300

    @Arthur-Ciliberti dashboard

--- js/dashboard_geral.js
+++ js/dashboard_geral.js
@@ -1,24 +1,24 @@
-// let ctx = document.querySelector('#myChart').getContext('2d')
+let ctx = document.querySelector('#myChart').getContext('2d')
 
-// const labels = ['time1', 'time2', 'time3', 'time4'];
+const labels = ['time1', 'time2', 'time3', 'time4'];
 
-// const points = [100, 121, 300, 290];
-// const color = '#03fa6e';
+const points = [100, 121, 300, 290];
+const color = '#03fa6e';
 
-// const data ={
-//     labels: labels,
-//     datasets : [
-//         {
-//             label:'pontuacao',
-//             data: points,
-//             borderColor: color,
-//             backgroundColor: color
-//         }
-//     ]
-// }
-// const config = {
-//     type: 'line',
-//     data: data
-// }
+const data ={
+    labels: labels,
+    datasets : [
+        {
+            label:'pontuacao',
+            data: points,
+            borderColor: color,
+            backgroundColor: color
+        }
+    ]
+}
+const config = {
+    type: 'line',
+    data: data
+}
 
-// let myChart = new CharacterData(ctx, config);
+let myChart = new CharacterData(ctx, config);
