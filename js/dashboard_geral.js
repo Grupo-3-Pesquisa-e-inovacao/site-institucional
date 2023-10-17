d1cc10300fd199b59cc35b9bc9f6e691db2615e9
Ciliberti <arthur.coliveira@sptech.school>
2023-10-17 12:09:29 -0300

    @Arthur-Ciliberti Finalização dashboard geral

--- js/dashboard_geral.js
+++ js/dashboard_geral.js
@@ -1,32 +1,87 @@
-let ctx = document.querySelector('#myChart')
-
-const labels = ['time1', 'time2', 'time3', 'time4'];
-
-const points = [100, 121, 300, 290];
-const color = '#03fa6e';
-
-let grafico = new Chart(ctx, {
-type: "bar",
-data: {
-    labels: labels,
+// Gráfico de Linha
+var lineData = {
+    labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
     datasets: [
         {
-            label: "Acompanhamento dos votos:",
-            data: points,
-            backgroundColor: [`${color}`,
-            `${color}`,
-            `${color}`,
-            `${color}`],
-            borderWidth: 1,
-        },
-    ],
-},
-options: {
-    scales: {
-        responsive: true,
-        y: {
-            beginAtZero: true,
-        },
-    },
-},
+            label: "Usos",
+            data: [40, 20, 150, 100, 40, 260, 356, 200, 170, 160, 190, 340],
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1,
+        }
+    ]
+};
+
+var lineCtx = document.getElementById('myLineChart').getContext('2d');
+var lineChart = new Chart(lineCtx, {
+    type: 'line',
+    data: lineData
 });
+
+// Gráfico de Rosca
+var doughnutData = {
+    // labels: ["Operação Normal", "Operação em Alerta", "Operação em Risco"],
+    datasets: [
+        {
+            data: [18, 4, 8],
+            backgroundColor: ['#109A68', '#DAC617', '#FF0000'],
+            borderRadius: 5,
+        }
+    ]
+};
+
+var doughnutCtx = document.getElementById('myDoughnutChart').getContext('2d');
+var doughnutChart = new Chart(doughnutCtx, {
+    type: 'doughnut',
+    data: doughnutData,
+});
+
+
+var horizontalBarCtx = document.getElementById('myHorizontalBarChart').getContext('2d');
+
+var horizontalBarData = {
+    labels: [""],
+    datasets: [
+        {
+            label: "Em uso",
+            data: [30],
+            backgroundColor: '#108456',
+            barThickness: 20,
+            borderRadius: 10,
+            barPercentage: 1,
+        },
+        {
+            label: "Desligados",
+            data: [13],
+            backgroundColor: '#7ACBAD',
+            barThickness: 20,
+            borderRadius: 10,
+            barPercentage: 1,
+        }
+    ]
+};
+
+var horizontalBarChart = new Chart(horizontalBarCtx, {
+    type: 'bar',
+    data: horizontalBarData,
+    options: {
+        indexAxis: 'y',
+        scales: {
+            x: {
+                grid: {
+                    display: false // Remove a grade de fundo no eixo X
+                }
+            },
+
+            y: {
+                beginAtZero: true,
+                grid: {
+                    display: false // Remove a grade de fundo no eixo Y
+                }
+
+            }
+        }
+    }
+});
+
+// CARROSSEL
